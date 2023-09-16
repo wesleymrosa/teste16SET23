b95c713c0b9ad7babd7b9f66c8419d54305b9b8a
Wesley Rosa <weleymrosa@gmail.com>
2023-09-16 19:10:02 -0300

    Primeiro commit

---
+++ src/main/java/com/wesley/teste16SET23/Teste16Set23Application.java
@@ -0,0 +1,13 @@
+package com.wesley.teste16SET23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teste16Set23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teste16Set23Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wesley/teste16SET23/Teste16Set23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesley.teste16SET23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Teste16Set23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
